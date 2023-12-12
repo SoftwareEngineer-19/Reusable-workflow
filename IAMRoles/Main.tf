397d87298e2da02cd04a74825b3dfc8eac6868d3
Varish Ansari <varish@sustainabilityeconomics.com>
2023-12-12 15:50:49 +0530

    iamroles updates

--- IAMRoles/Main.tf
+++ IAMRoles/Main.tf
@@ -7,10 +7,10 @@ terraform {
 
 	backend "remote" {
 		hostname = "app.terraform.io"
-		organization = "CloudQuickLabs"
+		organization = "AWS-Architet-org"
 
 		workspaces {
-			name = "AWSBackup"
+			name = "Test-workspace"
 		}
 	}
 }
---
+++ IAMRoles/test.tfvars
@@ -0,0 +1,1 @@
+role_name = "test_testing_role"
